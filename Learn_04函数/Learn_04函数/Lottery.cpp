486acafb78ab52ee30396b1fa11fd3fefc5191c6
yue.wang <yue.wang@puzzlecastle.co>
2020-08-24 10:24:59 +0800

    上传文件

---
+++ Learn_04函数/Learn_04函数/Lottery.cpp
@@ -0,0 +1,9 @@
+//
+//  Lottery.cpp
+//  Learn_04函数
+//
+//  Created by yue.wang on 2020/8/22.
+//  Copyright © 2020 yue.wang. All rights reserved.
+//
+
+#include "Lottery.hpp"
---
+++ Learn_05通讯录管理系统/Learn_05通讯录管理系统/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Learn_05通讯录管理系统
+//
+//  Created by yue.wang on 2020/8/22.
+//  Copyright © 2020 yue.wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
