a1a703cb54768d068621fc5f5460a6904d156937
Vision <33413143+visowY@users.noreply.github.com>
2020-12-25 22:26:36 +0800

    2222
    111

---
+++ Learn_01/Learn_01.cpp
@@ -0,0 +1,21 @@
+﻿// Learn_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score[10];
+    int score2[3] = { 1,2,3 };
+    int score3[] = { 1,2,3 };
+    int score4[10] = { 0 };
+
+    int length = sizeof(score4) / sizeof(score4[0]);
+    for (int i = 0; i < length; i++) {
+        cout << score4[i] << endl;
+    }
+
+    std::cout << "Hello World!\n";
+}
+
